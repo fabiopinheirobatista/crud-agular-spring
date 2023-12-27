9bbdda4fff3953b76c82d5005b1b8877f124e00c
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2023-12-27 20:05:48 -0300

    Adding components and Angular Material

--- crud-angular/src/app/app.routes.ts
+++ crud-angular/src/app/app.routes.ts
@@ -1,3 +1,9 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { ListComponentComponent } from './components/list-component/list-component.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: FirstComponentComponent },
+  { path: 'list', component: ListComponentComponent }
+];
