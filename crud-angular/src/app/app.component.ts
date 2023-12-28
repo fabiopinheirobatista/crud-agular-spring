cd7f53dd8b3625ac8ce267556b2d3511e28666ad
Fabio <42181125+fabiopinheirobatista@users.noreply.github.com>
2023-12-27 21:54:32 -0300

    Added Angular Material Components

--- crud-angular/src/app/app.component.ts
+++ crud-angular/src/app/app.component.ts
@@ -1,11 +1,14 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- crud-angular/src/app/app.routes.ts
+++ crud-angular/src/app/app.routes.ts
@@ -1,5 +1,4 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes } from '@angular/router';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ListComponentComponent } from './components/list-component/list-component.component';
 
